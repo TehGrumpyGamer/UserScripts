2376b8bada4cfdce3df8420c78a0cfda93c9e4a6
TehGrumpyGamer <tehgrumpygamer@gmail.com>
2017-01-15 12:20:05 -0600

    Initial script commit

---
+++ Gamdom.com Chat Enhancer/Chat Enhancer Script.js
@@ -0,0 +1,24 @@
+﻿// ==UserScript==
+// @id           Gamdom.com Chat Enhancer@https://github.com/TehGrumpyGamer/UserScripts
+// @name         Gamdom.com Chat Enhancer
+// @namespace    https://github.com/TehGrumpyGamer/UserScripts
+// @version      0.1
+// @author       TehGrumpyGamer (http://steamcommunity.com/id/tehgrumpygamer/)
+// @description  Provides enhancements to the chat section of Gamdom.com
+// @copyright    2016+ TehGrumpyGamer
+// @license      GNU GPLv3
+// @homepage     https://github.com/TehGrumpyGamer/UserScripts/tree/master/Gamdom.com%20Chat%20Enhancer#readme
+// @homepageURL  https://github.com/TehGrumpyGamer/UserScripts/tree/master/Gamdom.com%20Chat%20Enhancer#readme
+// @match        *://gamdom.com/
+// @exclude      *://gamdom.com/fa*
+// @exclude      *://gamdom.com/support*
+// @exclude      *://gamdom.com/leader*
+// @exclude      *://gamdom.com/terms*
+// @exclude      *://gamdom.com/trans*
+// @exclude      *://gamdom.com/name*
+// @exclude      *://gamdom.com/user*
+// @icon         https://gamdom.com/img/favicon.ico
+// @run-at       document-end
+// @require      http://code.jquery.com/jquery-latest.js
+// @require      https://raw.githubusercontent.com/bartaz/sandbox.js/master/jquery.highlight.js
+// ==/UserScript==
